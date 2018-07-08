728aa6f7aad6e574c7fc2e34cf006e60ffab1ae7
PrtmRyan10 <pryan1778@gmail.com>
2018-07-08 15:08:44 +0700

    add navbar index

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
 	<title>TRAVEL</title>
 
-	
+	<link href="bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="bootstrap/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+	<link href="bootstrap/css/sb-admin.css" rel="stylesheet">
 </head>
 <body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+        <a class="navbar-brand" href="index.html">TRAVEL</a>
 
 </body>
 </html>
