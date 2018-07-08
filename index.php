9c99c2a480432c6a8da3d5abbf6324d837e16d04
PrtmRyan10 <pryan1778@gmail.com>
2018-07-08 15:14:57 +0700

    add Beranda index

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>TRAVEL</title>
 
-	<link href="bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href="bootstrap/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-	<link href="bootstrap/css/sb-admin.css" rel="stylesheet">
+<head>
+    <title>TRAVEL</title>
+
+    <link href="bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <link href="bootstrap/css/sb-admin.css" rel="stylesheet">
 </head>
+
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
         <a class="navbar-brand" href="index.html">TRAVEL</a>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+                    <a class="nav-link" href="index.html">
+                        <i class="fa fa-home"></i>
+                        <span class="nav-link-text">BERANDA</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
 </body>
+
 </html>
