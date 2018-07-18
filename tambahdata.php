f067b685862a8096e948548b2b60ba8f16018a3c
Pratama Ryan <30280168+PrtmRyan10@users.noreply.github.com>
2018-07-18 21:22:37 +0700

    add file tambah data

---
+++ tambahdata.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TAMBAH DATA PEMESANAN</title>
+
+     <link href="bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <link href="bootstrap/css/sb-admin.css" rel="stylesheet">
+
+  </head>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  <body>
+    
+  </body>
+</html>
