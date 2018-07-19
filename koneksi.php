c38a2eca253113c5756a078c3ec8a6cf177a5d8a
Pratama Ryan <30280168+PrtmRyan10@users.noreply.github.com>
2018-07-19 10:05:25 +0700

    add file koneksi

---
+++ koneksi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Proses</title>
+  </head>
+  <body>
+    <?php
+      $host ="localhost";
+      $user="root";
+      $pass="";
+      $db="travel";
+
+      $koneksi = mysqli_connect($host,$user,$pass,$db);
+      
+     ?>
+  </body>
+</html>
