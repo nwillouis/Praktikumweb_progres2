fb38d2137f7c7844725c40e5c7d518a2bcfd0313
Pratama Ryan <30280168+PrtmRyan10@users.noreply.github.com>
2018-07-19 12:14:39 +0700

    change file index conten

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
 
       <div class="row">
         <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-primary o-hidden h-100">
+          <div class="card text-white bg-primary o-hidden h-50">
             <div class="card-body">
               <div class="card-body-icon">
               </div>
@@ -113,7 +113,7 @@
           </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-warning o-hidden h-100">
+          <div class="card text-white bg-warning o-hidden h-50">
             <div class="card-body">
               <div class="card-body-icon">
               </div>
@@ -122,7 +122,7 @@
           </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-success o-hidden h-100">
+          <div class="card text-white bg-success o-hidden h-50">
             <div class="card-body">
               <div class="card-body-icon">
               </div>
@@ -131,7 +131,7 @@
           </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-danger o-hidden h-100">
+          <div class="card text-white bg-danger o-hidden h-50">
             <div class="card-body">
               <div class="card-body-icon">
               </div>
