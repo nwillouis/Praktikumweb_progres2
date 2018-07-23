e9b1829f493f2a77a2f897de3773230de7c1e552
Yoesrilim <yoesrilcnblue@gmail.com>
2018-07-23 10:10:55 +0700

    add file hotel

---
+++ hotel.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>TRAVEL</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <link href="bootstrap/css/sb-admin.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+        <a class="navbar-brand" href="index.html"><img src="img/logo.jpg"></a>
+
+        <form>
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Search for...">
+                <span class="input-group-append">
+                    <button class="btn btn-primary" type="button">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+        </form>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="BERANDA">
+                    <a class="nav-link" href="index.php">
+                        <i class="fa fa-home"></i>
+                        <span class="nav-link-text">BERANDA</span>
+                    </a>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="DESTINATION">
+                    <a class="nav-link" href="Destination.php">
+                        <i class="fa fa-globe"></i>
+                        <span class="nav-link-text">DESTINATION</span>
+                    </a>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="TYPE TRANSPORTASION">
+                    <a class="nav-link" href="transportation.php">
+                        <i class="fa fa-plane"></i>
+                        <span class="nav-link-text">TYPE TRANSPORTASION</span>
+                    </a>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="HOTEL">
+                    <a class="nav-link" href="hotel.php">
+                        <i class="fa fa-bed"></i>
+                        <span class="nav-link-text">HOTEL</span>
+                    </a>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="BOOKING">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-handshake-o"></i>
+                        <span class="nav-link-text">BOOKING</span>
+                    </a>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="TICKET">
+                    <a class="nav-link" href="index.html">
+                        <i class="fa fa-ticket"></i>
+                        <span class="nav-link-text">TICKET</span>
+                    </a>
+                </li>
+                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="BOOKING CHECK">
+                    <a class="nav-link" href="select-booking.php">
+                        <i class="fa fa-tag"></i>
+                        <span class="nav-link-text">BOOKING CHECK</span>
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav sidenav-toggler">
+                <li class="nav-item">
+                    <a class="nav-link text-center" id="sidenavToggler">
+                        <i class="fa fa-star"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="content-wrapper">
+    <div class="container-fluid">
+      <!-- Breadcrumbs-->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">My Dashboard</li>
+      </ol>
+      
+      <!-- Card Columns Example Social Feed-->
+        <div class="container">
+
+      <!-- Page Heading -->
+      <i class="fa fa-bed"></i> Best Hotel </div>
+      <hr class="mt-2">
+
+      <div class="row">
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/hotelbromo.jpg" height="220"></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Hotel Bromo</a>
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/hotellabuanbajo.jpg" height="220"></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Hotel Labuan Bajo</a>
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/hotelrajaampat.jpg" height="220"></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Hotel Raja Ampat</a>
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/hotel.jpg" height="220"></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Hotel Nusa Penida</a>
+              </h4>
+            </div>
+          </div>
+        </div>
+</body>
+
+</html>
